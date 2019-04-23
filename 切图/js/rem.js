d536cb5229234d0f412e1756ce3d3a4e0cf9bbda
Allen <allenkis1990@163.com>
2019-04-23 17:41:14 +0800

    design

--- 切图/js/rem.js
+++ 切图/js/rem.js
@@ -3,7 +3,7 @@ let baseNum = 100;
 function setRootFontSize() {
     let winWidth = document.documentElement.clientWidth || document.body.clientWidth;
     let html = document.getElementsByTagName('html')[0];
-    html.style.fontSize = winWidth / designWidth * baseNum + 'px';
+    html.style.fontSize = (winWidth / designWidth) * baseNum + 'px';
     console.log(html.style.fontSize);
 }
 setRootFontSize();
