7ed6fce0a10b1d79b301578a6003199df8f2e57a
Allen <allenkis1990@163.com>
2019-12-05 16:45:55 +0800

    响应式设计稿切图

---
+++ 切图/纯手机/js/rem.js
@@ -0,0 +1,13 @@
+let designWidth = 750;
+let baseNum = 100;
+function setRootFontSize() {
+    let winWidth = document.documentElement.clientWidth || document.body.clientWidth;
+    let html = document.getElementsByTagName('html')[0];
+    html.style.fontSize = (winWidth / designWidth) * baseNum + 'px';
+    console.log(html.style.fontSize);
+}
+setRootFontSize();
+
+window.addEventListener('resize',(e)=>{
+    setRootFontSize();
+})
