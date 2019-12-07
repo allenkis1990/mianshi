eaf37dda01a7186f399ec5fa6b7a3ce2705c636e
allenkis1990 <allenkis1990@163.com>
2019-12-07 11:46:59 +0800

    123

--- 切图/响应式/js/rem.js
+++ 切图/响应式/js/rem.js
@@ -1,3 +1,11 @@
+
+
+//响应式实现思路
+// 媒体查询定义container的宽度
+// 如果设备宽度大于等于900全部都按html fontsize 100px来算rem
+// 如果设备小于900就按(winWidth / designWidth) * baseNum + 'px'来换算
+// 高度可以用rem  宽度尽量用百分比  太小的单位可以直接用px如边框
+// 最后再用媒体查询修修补补
 let designWidth = 900;
 let baseNum = 100;
 function setRootFontSize() {
