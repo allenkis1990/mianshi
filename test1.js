c1658273726f2586bdab113be40859ed73f7302e
Allen <allenkis1990@163.com>
2020-02-11 23:53:27 +0800

    高阶函数实例

--- test1.js
+++ test1.js
@@ -1,17 +1,12 @@
-var x = 3,
-    obj = {x:5}
-    obj.fn = (function(){
-        this.x *= ++x
-        return function(y){
-            this.x*=(++x)+y
-            console.log(x);
-        }
-    })()
-
-var fn = obj.fn //x=4
-obj.fn(6)//x=5  obj.x = 55  x=6
-fn(4) //x=7  x=77
-console.log(obj.x, x);//55 77
-
-
-
+fn()
+function fn(){
+    console.log(1);
+}
+fn()
+function fn(){
+    console.log(2);
+}
+fn()
+function fn(){
+    console.log(3);
+}
